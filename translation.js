8d661f61fb62517b95a5a79b42920192d20da695
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-03-03 09:28:10 +0100

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,19 +1,19 @@
 var TRANSLATION_MAP = {
   "alles zurück auf die Hand":{
     "english":"Unset selected letters"
-    "italian":""
-    "esperanto":""
+    "italian":"Togliere le lettere selezionate"
+    "esperanto":"Malvalidigi elektitajn literojn"
   },
   "Buchstaben tauschen (passen)":{
     "german":"passen",
     "english":"skip turn"
-    "italian":""
-    "esperanto":""
+    "italian":"passa il turno"
+    "esperanto":"transdoni vicon"
   },
   "Übrige Buchstaben":{
     "english":"Letters left in stash"
-    "italian":""
-    "esperanto":""
+    "italian":"Lettere rimaste"
+    "esperanto":"Literoj disponeblaj"
   },
   "Deine Buchstaben":{
     "english":"Your letters"
@@ -22,33 +22,33 @@ var TRANSLATION_MAP = {
   },
   "spielen":{
     "english":"set letters"
-    "italian":""
-    "esperanto":""
+    "italian":"convalida"
+    "esperanto":"validigi"
   },
   "Dreifacher Buchstabenwert":{
     "english":"triple letter value"
-    "italian":""
-    "esperanto":""
+    "italian":"triplica valore lettera"
+    "esperanto":"triobligi literan valoron"
   },
   "Doppelter Buchtstabenwert":{
     "english":"double letter value"
-    "italian":""
-    "esperanto":""
+    "italian":"raddoppia valore lettera"
+    "esperanto":"duobligi literan valoron"
   },
   "Dreifacher Wortwert":{
     "english":"triple word value"
-    "italian":""
-    "esperanto":""
+    "italian":"triplica valore parola"
+    "esperanto":"triobligi vortan valoron"
   },
   "Doppelter Wortwert":{
     "english":"double word value"
-    "italian":""
-    "esperanto":""
+    "italian":"raddoppia valore parola"
+    "esperanto":"duobligi vortan valoron"
   },
   "Welchen Buchstaben möchtest du hier setzen?":{
     "english":"Which letter do you want to set?"
-    "italian":""
-    "esperanto":""
+    "italian":"Quale lettera vuoi inserire?"
+    "esperanto":"Kiun literon vi volas aldoni?"
   },
   "DU":{
     "english":"YOU"
@@ -62,8 +62,8 @@ var TRANSLATION_MAP = {
   },
   "Wähle die Buchstaben aus, welche Du tauschen möchtest, dann klicke hier":{
     "english":"Choose the letters you want to swap, then click here"
-    "italian":""
-    "esperanto":""
+    "italian":"Scegli le lettere da scambiare, poi clicca qui"
+    "esperanto":"Elektu la literojn vi intencas ŝanĝi, poste alklaku ĉi tie"
   },
   "Das Spiel ist aus.":{
     "english":"Game is over."
@@ -107,8 +107,8 @@ var TRANSLATION_MAP = {
   },
   "für":{
     "english":"for"
-    "italian":""
-    "esperanto":""
+    "italian":"per"
+    "esperanto":"kontraŭ"
   },
   "punkte":{
     "english":"points"
@@ -117,51 +117,51 @@ var TRANSLATION_MAP = {
   },
   "Legen des ersten Wortes":{
     "english":"Inserting the first word"
-    "italian":""
-    "esperanto":""
+    "italian":"Inserire la prima parola"
+    "esperanto":"Aldoni la unuan vorton"
   },
   "Der erste Spieler bildet ein Wort aus mindestens zwei Buchstaben. Er platziert es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.":{
     "german":"Bilde ein Wort aus mindestens zwei Buchstaben und platziere es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.",
     "english":"Form a word of at least two letters and place it on the board in such a way that a tile covers the field in the middle of the board. Words can be placed horizontally or vertically. Words must not be placed diagonally. All subsequent letters must be placed in a continuous line, either horizontally or vertically."
-    "italian":""
-    "esperanto":""
+    "italian":"Forma una parola di almeno due lettere e posizionala sul tabellone in modo tale che una tessera copra il campo al centro del tabellone. Le parole possono essere posizionate orizzontalmente o verticalmente. Le parole non devono essere posizionate in diagonale. Tutte le lettere successive devono essere posizionate su una linea continua, orizzontalmente o verticalmente."
+    "esperanto":"Formu vorton almenaŭ du literojn longa kaj aldonu ĝin al la tabulon tiel, ke ŝtoneto kovru la kampon meze de la tabulo. Vortoj povas esti aldonitaj horizontale aŭ vertikale. Vortoj ne devas esti aldonitaj diagonale. Ĉiuj postaj literoj devas esti aldonitaj laŭ sinsekva linio, horizontale aŭ vertikale."
   },
   "Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.":{
     "german":"Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Dabei gibt es 50 extra Punkte, wenn alle 7 Buchstabensteine von der Hand auf einmal benutzt werden. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.",
     "english":"The task of the players is to construct words from letter stones with different values and to interpret them in such a way that they are connected with each other according to the type of a crossword puzzle. Each player must strive to achieve the highest possible score by making words out of the available letters, the letter value of which, in combination with the bonus fields of the game plan, results in the highest possible score. There are 50 extra points if all 7 letters from your hand are used at once. The total score that can be achieved in a game is about 400 to 800 and more, depending on the players' ability to play."
-    "italian":""
-    "esperanto":""
+    "italian":"Il compito dei giocatori è quello di costruire parole da tessere con lettere con valori diversi e di interpretarle in modo tale che siano collegate tra loro in modo simile ad un cruciverba. Ogni giocatore deve sforzarsi di ottenere il punteggio più alto possibile ricavando parole dalle lettere disponibili, il cui valore, in combinazione con i campi bonus del piano di gioco, si traduce nel punteggio più alto possibile. Si guadagnano 50 punti extra se tutte e 7 le lettere della mano vengono usate contemporaneamente. Il punteggio totale che può essere raggiunto in un gioco è di circa 400-800 e più, a seconda della capacità di gioco dei giocatori."
+    "esperanto":"La tasko de la ludantoj estas konstrui vortojn el literaj ŝtonetoj kun malsamaj valoroj kaj interpreti ilin tiel, ke ili estas ligitaj inter si laŭ la speco de krucvortenigmo. Ĉiu ludanto devas klopodi atingi la plej altan eblan poentaron kreante vortojn el la disponeblaj literoj, kies litera valoro, kombine kun la kromaj kampoj de la ludplano, rezultu la plej altan poentaron. 50 kromaj poentoj estas gajnitaj se ĉiuj 7 literoj de via mano estas uzataj samtempe. La totala poentaro atingebla en ludo estas proksimume 400 ĝis 800 kaj pli, depende de la kapablo de la ludantoj."
   },
   "Felder für Buchstabenprämien":{
     "english":"Letter Award Fields"
-    "italian":""
-    "esperanto":""
+    "italian":"Caselle punti extra lettera"
+    "esperanto":"Krompoentaj literaj kampoj"
   },
   "Wird ein Buchstabe auf ein hellblaues Feld gesetzt, so verdoppelt sich sein Wert. Wird ein Buchstabe auf ein dunkelblaues Feld gesetzt, so verdreifacht sich sein Wert.":{
     "english":"If a letter is placed on a light blue field, its value doubles. If a letter is placed on a dark blue field, its value triples."
-    "italian":""
-    "esperanto":""
+    "italian":"Se una lettera viene posizionata su una casella azzurra, il suo valore raddoppia. Se una lettera viene posizionata su una casella blu scuro, il suo valore triplica."
+    "esperanto":"Se litero estas metita sur helbluan kampon, ĝia valoro duobliĝas. Se litero estas metita sur malhelbluegan kampon, ĝia valoro triobliĝas."
   },
   "Felder für Wortprämien":{
     "english":"Word Premium Fields"
-    "italian":""
-    "esperanto":""
+    "italian":"Caselle punti extra parola"
+    "esperanto":"Krompoentaj vortaj kampoj"
   },
   "Wird ein Buchstabe auf ein hellrotes Feld gesetzt, so verdoppelt sich der Wert des gesamten Wortes. Wird ein Buchstabe auf ein dunkelrotes Feld gesetzt, so verdreifacht sich der Wert des gesamten Wortes.":{
     "english":"If a letter is placed on a light red field, the value of the entire word doubles. If a letter is placed on a dark red field, the value of the entire word is tripled."
-    "italian":""
-    "esperanto":""
+    "italian":"Se una lettera viene posizionata su una casella rosso chiaro, il valore dell'intera parola viene raddoppiato. Se una lettera viene posizionata su una casella rosso scuro, il valore dell'intera parola viene triplica."
+    "esperanto":"Se litero estas metita sur helruĝan kampon, la valoro de la tuta vorto duobliĝas. Se litero estas metita sur malhelruĝegan kampon, la valoro de la tuta vorto triobliĝas."
   },
   "Austausch von Buchstabensteinen":{
     "english":"Replacement of letter stones"
-    "italian":""
-    "esperanto":""
+    "italian":"Rimpiazzare tessere"
+    "esperanto":"Anstataŭigi ŝtonetojn"
   },
   "Ein Spieler hat die Möglichkeit einen oder all seine Buchstabensteine auszutauschen, wenn er an der Reihe ist. Er legt die Buchstabensteine mit dem Buchstaben nach unten zur Seite und zieht die gleiche Anzahl neuer Buchstabensteine aus dem Beutel. Die zur Seite gelegten Buchstabensteine werden dann in den Beutel gelegt und mit den anderen Steinen gemischt. Er muss diese Runde dann aussetzen.":{
     "german":"Du hast die Möglichkeit einen oder alle Buchstabensteine auszutauschen. Wähle die zu tauschenden Buchstaben aus, die gleiche Anzahl neuer Buchstabensteine bekommst du aus den übrigen Buchstaben. Die abgelegten Buchstabensteine werden dann zurück zum Vorrat gegeben. Du musst diese Runde dann aussetzen.",
     "english":"You have the possibility to replace one or all of your letter stones when it is your turn. Select the letter stones to be abandoned. The same number of new letter stones will be drawn form the stash. The abandoned letter stones are then placed in the stash. You must then suspend this round."
-    "italian":""
-    "esperanto":""
+    "italian":"Hai la possibilità di sostituire una o tutte le tessere quando è il tuo turno. Seleziona le tessere da eliminare. Lo stesso numero di nuove tessere verrà estratto dalla scorta. Le tessere eliminate verranno quindi messe nella scorta. È quindi necessario passare il turno."
+    "esperanto":"Vi havas la eblecon anstataŭigi unu aŭ ĉiujn viajn leterŝtonetojn kiam venas via vico. Elektu la forlasotajn literojn. La sama nombro da novaj literaj ŝtonetoj estos eltirita de la literujo. La forlasitaj leterŝtonetoj tiam estos metitaj en la literujon. Vi devas tiam ĉesigi ĉi tiun vicon."
   },
   "Du gewinnst.":{
     "english":"You win."
