27fde5634d0874c73c426b0ae2c5222e836f99d7
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-03-03 09:00:06 +0100

    Update translation.js
    Added Italian and Esperanto languages

--- translation.js
+++ translation.js
@@ -1,108 +1,176 @@
 var TRANSLATION_MAP = {
   "alles zurück auf die Hand":{
     "english":"Unset selected letters"
+    "italian":""
+    "esperanto":""
   },
   "Buchstaben tauschen (passen)":{
     "german":"passen",
     "english":"skip turn"
+    "italian":""
+    "esperanto":""
   },
   "Übrige Buchstaben":{
     "english":"Letters left in stash"
+    "italian":""
+    "esperanto":""
   },
   "Deine Buchstaben":{
     "english":"Your letters"
+    "italian":"Le tue lettere"
+    "esperanto":"Viaj literoj"
   },
   "spielen":{
     "english":"set letters"
+    "italian":""
+    "esperanto":""
   },
   "Dreifacher Buchstabenwert":{
     "english":"triple letter value"
+    "italian":""
+    "esperanto":""
   },
   "Doppelter Buchtstabenwert":{
     "english":"double letter value"
+    "italian":""
+    "esperanto":""
   },
   "Dreifacher Wortwert":{
     "english":"triple word value"
+    "italian":""
+    "esperanto":""
   },
   "Doppelter Wortwert":{
     "english":"double word value"
+    "italian":""
+    "esperanto":""
   },
   "Welchen Buchstaben möchtest du hier setzen?":{
     "english":"Which letter do you want to set?"
+    "italian":""
+    "esperanto":""
   },
   "DU":{
     "english":"YOU"
+    "italian":"TU"
+    "esperanto":"VI"
   },
   "KI":{
     "english":"AI"
+    "italian":"I.A."
+    "esperanto":"A.I."
   },
   "Wähle die Buchstaben aus, welche Du tauschen möchtest, dann klicke hier":{
     "english":"Choose the letters you want to swap, then click here"
+    "italian":""
+    "esperanto":""
   },
   "Das Spiel ist aus.":{
     "english":"Game is over."
+    "italian":"Fine del gioco."
+    "esperanto":"Ludo fininta."
   },
   "Wähle die Stärke des Computers":{
     "english":"Choose the strength of the ai"
+    "italian":"Scegli la forza dell'i.a."
+    "esperanto":"Elekti forton de a.i."
   },
   "schwach":{
     "english":"weak"
+    "italian":"debole"
+    "esperanto":"malforta"
   },
   "normal":{
     "english":"normal"
+    "italian":"normale"
+    "esperanto":"normala"
   },
   "stark":{
     "english":"strong"
+    "italian":"forte"
+    "esperanto":"forta"
   },
   "sehr stark":{
     "english":"very strong"
+    "italian":"molto forte"
+    "esperanto":"fortega"
   },
   "sehr sehr stark":{
     "english":"very very strong"
+    "italian":"fortissima"
+    "esperanto":"fortegega"
   },
   "unbesiegbar":{
     "english":"unbeatable"
+    "italian":"imbattibile"
+    "esperanto":"nevenkebla"
   },
   "für":{
     "english":"for"
+    "italian":""
+    "esperanto":""
   },
   "punkte":{
     "english":"points"
+    "italian":"punti"
+    "esperanto":"poentoj"
   },
   "Legen des ersten Wortes":{
     "english":"Inserting the first word"
+    "italian":""
+    "esperanto":""
   },
   "Der erste Spieler bildet ein Wort aus mindestens zwei Buchstaben. Er platziert es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.":{
     "german":"Bilde ein Wort aus mindestens zwei Buchstaben und platziere es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.",
     "english":"Form a word of at least two letters and place it on the board in such a way that a tile covers the field in the middle of the board. Words can be placed horizontally or vertically. Words must not be placed diagonally. All subsequent letters must be placed in a continuous line, either horizontally or vertically."
+    "italian":""
+    "esperanto":""
   },
   "Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.":{
     "german":"Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Dabei gibt es 50 extra Punkte, wenn alle 7 Buchstabensteine von der Hand auf einmal benutzt werden. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.",
     "english":"The task of the players is to construct words from letter stones with different values and to interpret them in such a way that they are connected with each other according to the type of a crossword puzzle. Each player must strive to achieve the highest possible score by making words out of the available letters, the letter value of which, in combination with the bonus fields of the game plan, results in the highest possible score. There are 50 extra points if all 7 letters from your hand are used at once. The total score that can be achieved in a game is about 400 to 800 and more, depending on the players' ability to play."
+    "italian":""
+    "esperanto":""
   },
   "Felder für Buchstabenprämien":{
     "english":"Letter Award Fields"
+    "italian":""
+    "esperanto":""
   },
   "Wird ein Buchstabe auf ein hellblaues Feld gesetzt, so verdoppelt sich sein Wert. Wird ein Buchstabe auf ein dunkelblaues Feld gesetzt, so verdreifacht sich sein Wert.":{
     "english":"If a letter is placed on a light blue field, its value doubles. If a letter is placed on a dark blue field, its value triples."
+    "italian":""
+    "esperanto":""
   },
   "Felder für Wortprämien":{
     "english":"Word Premium Fields"
+    "italian":""
+    "esperanto":""
   },
   "Wird ein Buchstabe auf ein hellrotes Feld gesetzt, so verdoppelt sich der Wert des gesamten Wortes. Wird ein Buchstabe auf ein dunkelrotes Feld gesetzt, so verdreifacht sich der Wert des gesamten Wortes.":{
     "english":"If a letter is placed on a light red field, the value of the entire word doubles. If a letter is placed on a dark red field, the value of the entire word is tripled."
+    "italian":""
+    "esperanto":""
   },
   "Austausch von Buchstabensteinen":{
     "english":"Replacement of letter stones"
+    "italian":""
+    "esperanto":""
   },
   "Ein Spieler hat die Möglichkeit einen oder all seine Buchstabensteine auszutauschen, wenn er an der Reihe ist. Er legt die Buchstabensteine mit dem Buchstaben nach unten zur Seite und zieht die gleiche Anzahl neuer Buchstabensteine aus dem Beutel. Die zur Seite gelegten Buchstabensteine werden dann in den Beutel gelegt und mit den anderen Steinen gemischt. Er muss diese Runde dann aussetzen.":{
     "german":"Du hast die Möglichkeit einen oder alle Buchstabensteine auszutauschen. Wähle die zu tauschenden Buchstaben aus, die gleiche Anzahl neuer Buchstabensteine bekommst du aus den übrigen Buchstaben. Die abgelegten Buchstabensteine werden dann zurück zum Vorrat gegeben. Du musst diese Runde dann aussetzen.",
     "english":"You have the possibility to replace one or all of your letter stones when it is your turn. Select the letter stones to be abandoned. The same number of new letter stones will be drawn form the stash. The abandoned letter stones are then placed in the stash. You must then suspend this round."
+    "italian":""
+    "esperanto":""
   },
   "Du gewinnst.":{
     "english":"You win."
+    "italian":"Hai vinto."
+    "esperanto":"Vi venkis."
   },
   "Du verlierst.":{
     "english":"You loose."
+    "italian":"Hai perso."
+    "esperanto":"Vi malvenkis."
   }
 };
